c9df5288a4d01ef3bc3938b8abdf448f5433802f
amberm <nekosenpai@gmail.com>
2019-11-19 16:21:40 -0700

    can go no further with all the npm warnings and CORS issues the backend has

--- tinycakes/src/Orders.js
+++ tinycakes/src/Orders.js
@@ -17,6 +17,13 @@ function Orders() {
             console.log(error)
         })
 
+        //xhr.js:166 Access to XMLHttpRequest at 'localhost:4000/cupcakes/orders' from origin 'http://localhost:3000' has been blocked by CORS policy: Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https.
+        
+        // not sure how to tell if it displays since I have CORS errors all over from a localhost...
+        // plus the instructs in the readme don't work anyway, npm install failed several things, and the build-ts command never worked, so perhaps that's why...
+        // it all works in postman though... no CORS issues there... so I'll try some other things, but this might be where it ends for now...
+
+        // I could fix it, but I'd have to modify the backend portion of this.
     }, [])
 
 
