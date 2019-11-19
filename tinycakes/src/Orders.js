9f3a6ec52f5943ad2f0aea6c982aef12cd9d42a8
amberm <nekosenpai@gmail.com>
2019-11-19 16:08:20 -0700

    running into cors errors locally, cross origin should not have been a problem locally, but I can't change the backend code to fix it

--- tinycakes/src/Orders.js
+++ tinycakes/src/Orders.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
 import { Route, Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Orders() {
 
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        axios.get('localhost:4000/cupcakes/orders')
+        .then( data => {
+            console.log('order data',data)
+            
+        })
+        .catch( error => {
+            console.log(error)
+        })
+
+    }, [])
 
 
-    
   return (
     <div className="Orders">
      
